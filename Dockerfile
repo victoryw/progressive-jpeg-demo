5bbe10a8f6003c9f4324ef9cb5c202c3f7617526
tianyawy <tianya_wy@hotmail.com>
2017-01-09 02:29:14 +0800

    [victoryw] #n/a use the gulp to build progrssive jpg, and the recompress file is little low

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM httpd:2.4
-COPY ./index.html /usr/local/apache2/htdocs/
-COPY ./single-image/ /usr/local/apache2/htdocs/single-image/
-COPY ./images/ /usr/local/apache2/htdocs/images/
+COPY ./dist/index.html /usr/local/apache2/htdocs/
+COPY ./dist/single-image/ /usr/local/apache2/htdocs/single-image/
+COPY ./dist/images/ /usr/local/apache2/htdocs/images/
 COPY ./httpd.conf /usr/local/apache2/conf/httpd.conf
 
 EXPOSE 80
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,63 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin'
 import imageminJpegRecompress from 'imagemin-jpeg-recompress'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminJpegtran from 'imagemin-jpegtran'
+import rm from  'gulp-rm'
+import rename from 'gulp-rename'
 
-gulp.task('hello', ()=>{
+gulp.task(function hello(done){
     console.log("this is test task");
+    done();
 });
 
-gulp.task('default', () =>
-    gulp.src('origin-jpg/*')
-        .pipe(imagemin([imagemin.jpegtran({progressive: true}), imageminJpegRecompress({min: 95, max:95})]))
-        .pipe(gulp.dest('dist/images'))
+
+let cleanDist =() => {
+    return gulp.src('dist/**/*', {read: false})
+        .pipe(rm());
+};
+
+let copyHtml =() => {
+    return   gulp.src('index.html')
+        .pipe(gulp.dest('dist/'));
+};
+
+let copySingleHtml =() => {
+    return   gulp.src('single-image/*.html')
+        .pipe(gulp.dest('dist/single-image/'));
+};
+
+let originJpg =() => {
+    return   gulp.src('origin-jpg/mountain.jpg')
+        .pipe(rename('mountain-base-line.jpg'))
+        .pipe(gulp.dest('dist/images'));
+}
+
+
+let jptran = () =>{
+    return gulp.src('origin-jpg/mountain.jpg')
+        .pipe(imagemin([imageminJpegtran({progressive: true})]))
+        .pipe(rename('mountain-progressive-jptran.jpg'))
+        .pipe(gulp.dest('dist/images/'));
+};
+
+let jptranCompress =() =>{
+    return  gulp.src('origin-jpg/mountain.jpg')
+        .pipe(imagemin([imageminJpegtran({progressive: true}),imageminJpegRecompress({min: 98, max:98})]))
+        .pipe(rename('mountain-progressive-jptran-recompress.jpg'))
+        .pipe(gulp.dest('dist/images/'));
+};
+
+let mozjpeg =() =>{
+  return gulp.src('origin-jpg/mountain.jpg')
+      .pipe(imagemin([imageminMozjpeg({progressive: true,quality: 95})]))
+      .pipe(rename('mountain-progressive-moz.jpg'))
+      .pipe(gulp.dest('dist/images/'));
+};
+
+
+
+export default gulp.series(
+    cleanDist,
+    gulp.parallel(copyHtml,copySingleHtml,jptran,mozjpeg,jptranCompress,originJpg)
 );
